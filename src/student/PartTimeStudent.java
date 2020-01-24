7404418b85dc668dca02f7c607468681dc5b5280
michaelhennessey <hennesmi@sherdiancollege.ca>
2020-01-24 11:40:15 -0500

    page for part time student

---
+++ src/student/PartTimeStudent.java
@@ -0,0 +1,15 @@
+package student;
+
+/**
+ * This class ++++Insert Description Here++++
+ * 
+ * @author Michael Hennessey
+ */
+public class PartTimeStudent extends Student{
+     
+    private int hoursOfStudy;
+    
+    public PartTimeStudent(String givenName, String givenID){
+        super(givenName, givenID);}
+
+}
